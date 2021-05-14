712b4f6efb9c2283f227c2a7fa4848c612509129
avulalakshman <learnwithlakshman@gmail.com>
2021-05-14 07:56:33 +0530

    Added bubble sort implementation

--- src/com/careerit/cj/basics/arrays/ArrayOperations.java
+++ src/com/careerit/cj/basics/arrays/ArrayOperations.java
@@ -31,7 +31,16 @@ public class ArrayOperations {
 	}
 
 	public int[] sort(int arr[]) {
-
-		return null;
+		int n = arr.length;
+		for (int i = 0; i < n - 1; i++) {
+			for (int j = 0; j < n - i - 1; j++) {
+				if (arr[j] > arr[j + 1]) {
+					int temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+				}
+			}
+		}
+		return arr;
 	}
 }
