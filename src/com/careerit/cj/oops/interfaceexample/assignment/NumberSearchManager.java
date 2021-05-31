19cca2d16e984854edcc00e0e4854e2d1360326a
'avulalakshman' <'learnwithlakshman@gmail.com'>
2021-05-31 09:14:39 +0530

    Added Assignment

--- src/com/careerit/cj/oops/interfaceexample/assignment/NumberSearchManager.java
+++ src/com/careerit/cj/oops/interfaceexample/assignment/NumberSearchManager.java
@@ -19,6 +19,7 @@ class UserData {
 	int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 16, 23, 45, 65, 78, 12, 13, 11, 15 };
 
 	int search(int key) {
+		
 		return elementSearch.search(arr, key);
 	}
 }
@@ -28,10 +29,10 @@ public class NumberSearchManager {
 	public static void main(String[] args) {
 		UserData obj = new UserData();
 		int res = obj.search(10);
-		if(res == -1) {
+		if (res == -1) {
 			System.out.println("Element is not found");
-		}else {
-			System.out.println("Element is found at :"+res);
+		} else {
+			System.out.println("Element is found at :" + res);
 		}
 	}
 }
